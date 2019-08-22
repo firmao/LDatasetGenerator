7880fdde5ded75f1d0fdd285818b5ff658a6bfe0
Andre Valdestilhas <firmao@gmail.com>
2019-08-22 22:32:26 +0200

    Add files via upload

--- src/main/java/test/testid/CSV2JSONEntities.java
+++ src/main/java/test/testid/CSV2JSONEntities.java
@@ -1,51 +1,84 @@
 package test.testid;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import au.com.bytecode.opencsv.CSVReader;
 
 public class CSV2JSONEntities {
 
 	static int countSkiped = 0;
+	final static Set<String> setErrors = new LinkedHashSet<String>();
+
 	public static void main(String[] args) throws IOException {
-		final String DIR_CSV = "cities_csv_en";
-		File files [] = (new File(DIR_CSV)).listFiles();
+		final String DIR_CSV = "cities_csv_en_norm1";
+		Experiment exp = new Experiment();
+		File files[] = (new File(DIR_CSV)).listFiles();
+		String sFileNT = null;
 		for (File file : files) {
-			File fDir = new File("outJsonFAMER7/" + file.getName().replaceAll(".csv", "") + "/");
+			File fDir = new File("JsonFAMERLODCities3/" + file.getName().replaceAll(".csv", "") + "/");
 			fDir.mkdirs();
-			
-			//Map<String, Map<String, String>> mEntities = parseCSV(file);
+
+			// Map<String, Map<String, String>> mEntities = parseCSV(file);
 			Map<String, Map<String, String>> mEntities = parseCSVOpen(file);
-			
+
 			printFiles(mEntities, fDir);
+			printErrors();
+			try {
+				sFileNT = exp.convertCSV2NT(file.getAbsolutePath());
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
 			System.out.println("file: " + file.getAbsolutePath());
+			System.out.println("file NT: " + sFileNT);
 			System.out.println("length: " + file.length());
 			System.out.println("SkipedLines: " + countSkiped);
+			System.out.println("City File Errors: " + setErrors.size());
 		}
-		
+
+	}
+
+	private static void printErrors() throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("ErrorsCSV2JsonFAMER.txt", "UTF-8");
+		for (String error : setErrors) {
+			writer.println(error);
+		}
+		writer.close();
 	}
 
 	private static void printFiles(Map<String, Map<String, String>> mEntities, File fDir) throws IOException {
 		for (Entry<String, Map<String, String>> entry : mEntities.entrySet()) {
 			String name = null;
-			if(entry.getKey().contains("http")){
+			if (entry.getKey().contains("http")) {
 				name = Util.getURLName(entry.getKey());
-			}else {
+			} else {
 				name = entry.getKey();
 			}
-			String fName = fDir +"/" + name + ".json";
+			name = name.replaceAll("/", "");
+			name = name.replaceAll(" ", "");
+			name = name.trim();
+			String fName = fDir + "/" + name + ".json";
 			File f = new File(fName);
-			if(!f.exists()) {
-				f.createNewFile();
+			try {
+				if (!f.exists()) {
+					f.createNewFile();
+				}
+			} catch (Exception e) {
+				setErrors.add(f.getAbsolutePath());
+				continue;
 			}
 			PrintWriter writer = new PrintWriter(fName, "UTF-8");
 			writer.println("{");
@@ -53,102 +86,98 @@ public class CSV2JSONEntities {
 			for (Entry<String, String> atts : entry.getValue().entrySet()) {
 				String key = atts.getKey();
 				String value = atts.getValue();
-				sbLines.append("\""+key + "\": " + "\"" + value + "\",\n");					
+				sbLines.append("\"" + key + "\": " + "\"" + value + "\",\n");
 			}
-			String content = sbLines.toString().substring(0, sbLines.length()-2);
+			String content = sbLines.toString().substring(0, sbLines.length() - 2);
 			writer.println(content);
 			writer.println("}");
 			writer.close();
 		}
-		
+
 	}
 
 	private static Map<String, Map<String, String>> parseCSVOpen(File file) {
 		Map<String, Map<String, String>> mEntities = new LinkedHashMap<String, Map<String, String>>();
 		CSVReader reader = null;
-        try
-        {
-            //Get the CSVReader instance with specifying the delimiter to be used
-//            if(file.getName().contains("geo")) {
-//            	reader = new CSVReader(new FileReader(file.getAbsolutePath()),';');
-//            } else {	
-            	reader = new CSVReader(new FileReader(file.getAbsolutePath()),',');
-//            }
-            String [] sValues;
-            String [] sNames = null;
-    		int count = 0;
-            while ((sValues = reader.readNext()) != null)
-            {
-            	if(count == 0) {
-    				sNames = sValues;
-    				count++;
-    				continue;
-    			}
-    
-    			if(sNames.length != sValues.length) {
-    				countSkiped++;
-    				continue;
-    			}
-    			String name = sValues[0];
-    			for(int i=1; i<sValues.length;i++) {
-    				String attName = sNames[i];
-    				String attValue = sValues[i];
-    				if(mEntities.get(name) != null) {
-    					mEntities.get(name).put(attName, attValue);
-    				} else {
-    					Map<String, String> mapAttributes = new LinkedHashMap<String, String>();
-    					mapAttributes.put(attName,attValue);
-    					mEntities.put(name, mapAttributes);
-    				}
-    			}
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-		
+		try {
+			// Get the CSVReader instance with specifying the delimiter to be used
+//			if (file.getName().contains("geonames")) {
+//				reader = new CSVReader(new FileReader(file.getAbsolutePath()), ';');
+//			} else {
+				reader = new CSVReader(new FileReader(file.getAbsolutePath()), ',');
+//			}
+			String[] sValues;
+			String[] sNames = null;
+			int count = 0;
+			while ((sValues = reader.readNext()) != null) {
+				if (count == 0) {
+					sNames = sValues;
+					count++;
+					continue;
+				}
+
+				if (sNames.length != sValues.length) {
+					countSkiped++;
+					continue;
+				}
+				String name = sValues[0];
+				for (int i = 1; i < sValues.length; i++) {
+					String attName = sNames[i];
+					String attValue = sValues[i];
+					if (mEntities.get(name) != null) {
+						mEntities.get(name).put(attName, attValue);
+					} else {
+						Map<String, String> mapAttributes = new LinkedHashMap<String, String>();
+						mapAttributes.put(attName, attValue);
+						mEntities.put(name, mapAttributes);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return mEntities;
 	}
-	
+
 	private static Map<String, Map<String, String>> parseCSV(File file) throws IOException {
 		List<String> fLines = Files.readAllLines(Paths.get(file.getAbsolutePath()));
 		Map<String, Map<String, String>> mEntities = new LinkedHashMap<String, Map<String, String>>();
-		String [] sNames = null;
+		String[] sNames = null;
 		int count = 0;
 		for (String sLine : fLines) {
 			String line = sLine.replaceAll("\",\"", "\t");
 			line = line.replaceAll("\"", "");
 			line = line.replaceAll(", ", ". ");
 			line = line.replaceAll(",", "\t");
-			
-			String [] sValues = line.split("\t");
-			//String [] sValues = line.split("\t");
-			if(count == 0) {
+
+			String[] sValues = line.split("\t");
+			// String [] sValues = line.split("\t");
+			if (count == 0) {
 				sNames = sValues;
 				count++;
 				continue;
 			}
 			String name = sValues[0];
-			//String attributes = line.replaceAll(name + ",", "");
-			if(sNames.length != sValues.length) {
+			// String attributes = line.replaceAll(name + ",", "");
+			if (sNames.length != sValues.length) {
 				countSkiped++;
 				continue;
 			}
-			for(int i=1; i<sValues.length;i++) {
+			for (int i = 1; i < sValues.length; i++) {
 				String attName = sNames[i];
 				String attValue = sValues[i];
-				if(mEntities.get(name) != null) {
+				if (mEntities.get(name) != null) {
 					mEntities.get(name).put(attName, attValue);
 				} else {
 					Map<String, String> mapAttributes = new LinkedHashMap<String, String>();
-					mapAttributes.put(attName,attValue);
+					mapAttributes.put(attName, attValue);
 					mEntities.put(name, mapAttributes);
 				}
 			}
---
+++ src/main/java/test/testid/GoldenTruthCSV.java
@@ -0,0 +1,175 @@
+package test.testid;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.text.similarity.JaroWinklerDistance;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.util.Set;
+
+public class GoldenTruthCSV {
+	public static final Map<String, String> mAlreadyCompared = new LinkedHashMap<String, String>();
+
+	public static void main(String[] args) throws Exception {
+		Set<String> ds = new LinkedHashSet<String>();
+		ds.add("cities_csv_en_norm1/dbpedia.csv");
+		ds.add("cities_csv_en_norm1/eea.europa.eu_most-populated-cities.csv");
+		ds.add("cities_csv_en_norm1/geonames-all-cities-with-a-population-1000.csv");
+		ds.add("cities_csv_en_norm1/lod.openlinksw.com_25sources.csv");
+		ds.add("cities_csv_en_norm1/mahnhein_searchJoins.webdatacommons.org.csv");
+		ds.add("cities_csv_en_norm1/wikidata.csv");
+		ds.add("cities_csv_en_norm1/yago.csv");
+		Set<String> dt = new LinkedHashSet<String>();
+		dt.addAll(ds);
+		long totalComparisons = ds.size() * dt.size();
+		System.out.println("Total datasets: " + ds.size());
+		int count = 0;
+		Set<Match> setMatches = new LinkedHashSet<Match>();
+		PrintWriter writer = new PrintWriter("matchScores.tsv", "UTF-8");
+		writer.println("DatasetSource\tDatasetTarget\tScore");
+		for (String source : ds) {
+			for (String target : dt) {
+				if (source.equals(target))
+					continue;
+				if (alreadyCompared(source, target))
+					continue;
+
+				System.out.println("Starting dataset comparison: " + count + " from " + totalComparisons);
+				System.out.println(source + "---" + target);
+				count++;
+				double dScore = getScore(source, target);
+				writer.println(source + "\t" + target + "\t" + dScore);
+				mAlreadyCompared.put(source, target);
+				mAlreadyCompared.put(target, source);
+			}
+		}
+		writer.close();
+		// printMatches(setMatches, 0.8);
+	}
+
+	private static void printMatches(Set<Match> setMatches, double threshold)
+			throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("matches.tsv", "UTF-8");
+		writer.println("DatasetSource\tSource\tDatasetTarget\tTarget\tScore");
+		for (Match match : setMatches) {
+			String dSource = null;
+			String dTarget = null;
+			String source = null;
+			String target = null;
+			for (Entry<String, String> entry : match.getSource().entrySet()) {
+				dSource = entry.getKey();
+				source = entry.getValue();
+			}
+			for (Entry<String, String> entry : match.getTarget().entrySet()) {
+				dTarget = entry.getKey();
+				target = entry.getValue();
+			}
+			writer.println(dSource + "\t" + source + "\t" + dTarget + "\t" + target + "\t" + match.getScore());
+		}
+		writer.close();
+	}
+
+	private static double getScore(String source, String target)
+			throws FileNotFoundException, UnsupportedEncodingException {
+		double scoreTotal = 0.0;
+		Set<Double> scores = new LinkedHashSet<Double>();
+
+		JaroWinklerDistance jaro = new JaroWinklerDistance();
+		Set<String> linesSource = getLines(source);
+		Set<String> linesTarget = getLines(target);
+		File fSource = new File(source);
+		File fTarget = new File(target);
+		String fileName = fSource.getName().replaceAll(".csv", "") + "---" + fTarget.getName().replaceAll(".csv", "");
+		PrintWriter writer = new PrintWriter("Entity_MatchScore_" + fileName + ".tsv", "UTF-8");
+		writer.println("EntitySource\tEntityTarget\tEntityScore");
+		int countEntityErrors = 0;
+		for (String lSource : linesSource) {
+			String[] sSource = lSource.split(",");
+			for (String lTarget : linesTarget) {
+				String[] sTarget = lTarget.split(",");
+				Set<Double> scoresEntity = new LinkedHashSet<Double>();
+				double scoreEntity = 0.0;
+				for (int i = 0; i < sSource.length; i++) {
+					double dSim = 0.0;
+					try {
+						String s = sSource[i].trim();
+						String t = sTarget[i].trim();
+						try {
+							double ds = Double.parseDouble(s);
+							double dt = Double.parseDouble(t);
+							if(ds == dt) dSim = 1.0;
+						} catch(Exception ex) {
+							dSim = jaro.apply(s, t);
+						}
+//						if(Util.isNumeric(sSource[i]) && isNumber(sTarget[i])) {
+//							dSim = simNumber(sSource[i], sTarget[i]);
+//						} else {
+//							dSim = jaro.apply(sSource[i], sTarget[i]);
+//						}
+						scoresEntity.add(dSim);
+					} catch (Exception e) {
+						countEntityErrors++;
+						continue;
+					}
+				}
+				for (Double sc : scoresEntity) {
+					scoreEntity += sc;
+				}
+				scoreEntity = scoreEntity / scoresEntity.size();
+				if(scoreEntity > 0.8) {
+					writer.println(sSource[0] + "\t" + sTarget[0] + "\t" + scoreEntity);
+				}
+				scores.add(scoreEntity);
+			}
+		}
+		writer.close();
+		for (Double sc : scores) {
+			scoreTotal += sc;
+		}
+		scoreTotal = scoreTotal / scores.size();
+		System.out.println("#Entity Errors: " + countEntityErrors);
+		return scoreTotal;
+	}
+
+	private static Set<String> getLines(String file) {
+		Set<String> ret = new LinkedHashSet<String>();
+		try {
+			List<String> lstLines = FileUtils.readLines(new File(file), "UTF-8");
+			int count = 0;
+			for (String line : lstLines) {
+				if(count > 0) {
+					ret.add(line);
+				}
+				count++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return ret;
+	}
+
+	private static boolean alreadyCompared(String source, String target) {
+		if (mAlreadyCompared.get(source) != null) {
+			if (mAlreadyCompared.get(source).equals(target)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+}
