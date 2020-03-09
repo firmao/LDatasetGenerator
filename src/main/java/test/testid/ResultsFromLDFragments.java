be17da66b848ec6e0dc67fc7b218fea181dee99e
Andre Valdestilhas <firmao@gmail.com>
2020-03-09 21:52:27 +0100

    Improvement of the documentation

--- src/main/java/test/testid/ResultsFromLDFragments.java
+++ src/main/java/test/testid/ResultsFromLDFragments.java
@@ -9,17 +9,29 @@ import java.util.List;
 public class ResultsFromLDFragments {
 
 	public static void main(final String[] args) throws IOException, InterruptedException {
+		String nodeDir = "/usr/local/bin/node";
+		String ldfClient = "Client.js/bin/ldf-client";
 		String fragmentsHost = "https://api.krr.triply.cc/datasets/krr/lod-a-lot/fragments";
         String sparql = "Select * where {?s ?p ?o} limit 10";
-		String result = getFromLDFragments(fragmentsHost, sparql);
+		String result = getFromLDFragments(fragmentsHost, sparql, nodeDir, ldfClient);
 		System.out.println(result);
     }
 	
-	public static String getFromLDFragments(String fragmentsHost, String sparql) throws IOException, InterruptedException {
+	
+	/**
+	 * @param fragmentsHost The host containing the LDFragments, i.g., https://api.krr.triply.cc/datasets/krr/lod-a-lot/fragments
+	 * @param sparql Can be a text containing the sparql query or a path to a file.sparql containing the query
+	 * @param nodeDir The path where node.js are installed, i.g. /usr/local/bin/node
+	 * @param ldfClient The path to the LDFragments client, i.g. Client.js/bin/ldf-client
+	 * @return The results from the sparql query in JSON format
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static String getFromLDFragments(String fragmentsHost, String sparql, String nodeDir, String ldfClient) throws IOException, InterruptedException {
 		List<String> commands = new ArrayList<String>();
-        commands.add("/usr/local/bin/node");
+        commands.add(nodeDir);
         //Add arguments
-        commands.add("Client.js/bin/ldf-client");
+        commands.add(ldfClient);
         commands.add(fragmentsHost);
         //commands.add("Client.js/queries/artists-york.sparql");
         commands.add(sparql);
