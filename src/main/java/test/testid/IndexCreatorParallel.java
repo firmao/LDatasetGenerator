d856a72691d0f0a1213e57b902dbbf2b3cae7530
Andre Valdestilhas <firmao@gmail.com>
2021-11-18 02:04:27 +0100

    improvement of matching

--- src/main/java/test/testid/IndexCreatorParallel.java
+++ src/main/java/test/testid/IndexCreatorParallel.java
@@ -113,7 +113,6 @@ public class IndexCreatorParallel {
 		try {
 			lstLines = Files.readAllLines(Paths.get(file));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
